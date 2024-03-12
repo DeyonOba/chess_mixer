478cf379f0c21d643d1dc18f6ff2b193738e76e2
Gideon Oba <deyonoba@gmail.com>
2024-03-13 00:50:14 +0100

    Add constants.py file

---
+++ chesspuzzler/constants.py
@@ -0,0 +1,13 @@
+"""Package Constants."""
+class Constant:
+    # Lichess API token Path
+    API_TOKEN_PATH = "lichess_api_token.json"
+
+    # Stockfish Engine Path
+    ENGINE_PATH = "stockfish_engine\stockfish-windows-x86-64-avx2.exe"
+
+    # Game Analysis Scan depth
+    SCAN_ENGINE_DEPTH = 15
+
+    # Puzzle Creation Depth
+    PUZZLE_ENGINE_DEPTH = 25
