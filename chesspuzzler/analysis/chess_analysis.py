c28fc72aa23294fe0e1abc55bda66216d61002b2
Gideon Oba <deyonoba@gmail.com>
2024-04-17 14:29:54 +0100

    refactor: logger, log degub statement, move board util functions

--- chesspuzzler/analysis/chess_analysis.py
+++ chesspuzzler/analysis/chess_analysis.py
@@ -7,43 +7,42 @@ import copy
 import sys
 import logging
 import pandas as pd
-from chesspuzzler.utils import FileManager
+from chesspuzzler.analysis.file_util import FileManager
 import chess
 import chess.pgn
 from chess.pgn import ChildNode
 import chess.engine
 from chess.engine import Cp, Mate, PovScore, InfoDict, SimpleEngine
-from chesspuzzler.constants import Constant
-import re
+from chesspuzzler.analysis.constants import Constant
+from chesspuzzler.analysis.board_util import symbol_uci_move
 from colorama import Fore, Back, Style
-from chesspuzzler.model import TrackEval, BestMovePair, BoardInfo
+from chesspuzzler.analysis.model import TrackEval, BestMovePair, BoardInfo
+from chesspuzzler.analysis.logger import configure_log
 
 # Create logging folder if it does not exist
 os.makedirs("./data/logging", exist_ok=True)
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-FORMAT = "%(funcName)s:%(levelname)s:%(message)s"
-formater = logging.Formatter(FORMAT)
-file_handler = logging.FileHandler(os.path.join(".", "data", "logging", "analysis.logging"))
-file_handler.setFormatter(formater)
-logger.addHandler(file_handler)
+logger = configure_log(__name__, "analysis.log")
 
 
 class EvaluationEngine:
     def __init__(
         self, node: ChildNode, engine: SimpleEngine,
-        info: InfoDict, prevScore: PovScore, turn: int 
+        info: InfoDict, prevScore: PovScore, 
+        prevpovScore: PovScore, turn: int 
     ) -> None:
         self.node = node
         self.engine = engine
         self.info = info
         self.prevScore = prevScore
+        self.prevpovScore = prevpovScore
         self.turn = turn
         self.comment = " "
         self.best_moves = []
        
     def position_eval(self) -> str:
         prev = TrackEval(self.prevScore, self.turn)
+        prevoppScore = TrackEval(self.prevScore, not self.turn)
+        prevpovScore = TrackEval(self.prevpovScore, self.turn)
 
         try:
             curr = TrackEval(self.info["score"], self.turn)
@@ -52,9 +51,13 @@ class EvaluationEngine:
 
         logger.info("--"*20)
         logger.info("SCORE DETAILS: ")
-        logger.info("Previous Move Scores:\ncp:  {}\nwdl: {}\nmate = {}".format(prev.cp, prev.wdl, prev.mate))
-        logger.info("Current Move Scores:\ncp: {}\nwdl: {}\nmate: {}".format(prev.cp, prev.wdl, prev.mate))
-        logger.info("--"*20)
+        logger.info("TURN: {}".format("White" if self.turn else "Black"))
+        logger.info("Full move number: {}\t Half move number: {}".format(self.node.board().fullmove_number,self.node.ply()))
+        logger.info("INDEX\t\t\tCP\tWDL\tMATE\t")
+        logger.info("Previous POV score     {}\t{}\t{}".format(prevpovScore.cp, prevpovScore.wdl, prevpovScore.mate))
+        logger.info("Previous Move Scores   {}\t{}\t{}".format(prev.cp, prev.wdl, prev.mate))
+        logger.info("Current Move Scores    {}\t{}\t{}".format(curr.cp, curr.wdl, curr.mate))
+
         
         self.current = curr
         # We don't expect majority of players to find mate in 30
@@ -80,19 +83,7 @@ class EvaluationEngine:
                 self.comment = "Lost mate and now in Mate..."
                 self.evaluation = "Blunder"
                 logger.info(f"Comment: {self.comment}\nJudgement:{self.evaluation}")
-                return "Blunder"
-            
-            # # Cp(0) < Cp(20) < Cp(400) < Mate(17) < Mate(3) < MateGiven
-            # elif prev.mateCreated and curr.noMateFound and Mate(prev.mate) >= mate_limit and advantage:
-            #     self.comment = "Missed mating chance created but you still have the advantage."
-            #     logger.info(f"Comment: {self.comment}")
-
-            #     if curr.wdl < prev.wdl:
-            #         logger.debug("Previous winning chance is greater the your current winning chance.")
-            #         return self.evaluate(curr.wdl, prev.wdl)
-            #     else:
-            #         logger.debug("Still winning but let's find the best move")
-            #         return self.further_analysis()
+                return "Blunder"        
             
             elif prev.mateCreated and curr.noMateFound:
                 if curr.cp > 999: return "Inaccuracy"
@@ -118,11 +109,6 @@ class EvaluationEngine:
                 self.comment = "Mating sequence created"
                 logger.info(f"Comment: {self.comment}")
                 return self.further_analysis()
-            
-            # elif curr.mate >= prev.mate and prev.mateCreated and Mate(prev.mate) > mate_limit:
-            #     self.comment = f"Fewer mating moves in {prev.mate} but now {curr.mate}"
-            #     logger.info(f"{self.comment}")
-            #     return self.further_analysis()
 
             elif curr.mate >= prev.mate and prev.mateCreated:
                 self.comment = f"Fewer mating moves in {prev.mate} but now {curr.mate}"
@@ -144,10 +130,17 @@ class EvaluationEngine:
             elif prev.cp < -700: return "Mistake"
             else: return "Blunder"
 
+        elif 0.3 > (1 - curr.wdl - prevoppScore.wdl) >= 0.1 and curr.wdl >= prevpovScore.wdl:
+            delta_wdl = 1 - curr.wdl - prevoppScore.wdl
+            if delta_wdl >= 0.2:
+                return "Mistake"
+            elif delta_wdl >= 0.1:
+                return "Inaccuracy"
+
         elif curr.wdl < prev.wdl:
             self.comment = "Previous winning chance is greater than current winning chance"
             logger.info(f"Comment: {self.comment}")
-            return self.evaluate(curr.wdl, prev.wdl)
+            return self.evaluate(curr.wdl, prevpovScore.wdl)
 
         else:
             self.comment = "Check for candidate moves and best move"
@@ -159,7 +152,6 @@ class EvaluationEngine:
     
     def evaluate(self, curr_wdl, prev_wdl):
         delta_wdl = prev_wdl - curr_wdl
-#         print("delta_wdl", delta_wdl)
         logger.debug("Delta win draw probability: {}".format(delta_wdl))
         if delta_wdl >= 0.3:
             self.comment = self.comment + "Blunder best move is  ."
@@ -169,35 +161,14 @@ class EvaluationEngine:
             self.comment = self.comment + "Mistake best move is  ."
             logger.info(f"{self.comment}")
             return "Mistake"
-        elif delta_wdl >= 0.1:
+        elif delta_wdl >= 0.5:
             self.comment = self.comment + "Made an inaccuracy you can consider moves like  ."
             logger.info(f"{self.comment}")
             return "Inaccuracy"
         else:
             self.comment = self.comment + "Check for best moves"
             return self.further_analysis()
-        
-def split_uci_moves(move: str) -> str:
-    # Pattern to split should be a non digit and a digit
-    PATTERN = r'(\D\d)'
-    
-    output = re.split(PATTERN, move)
-    # Remove empty string from the output
-    output = [s for s in output if s]
-    # The uci (universal chess interface) consists of two concatenated string
-    # positions the first is the original position of piece on board, 
-    # and the second is the move been made on the board.
-    
-    # Example
-    # d2d4 (Queen's opening)
-    # d2 is the original position of the pawn
-    # d4 is the move been made
-    return output[1]
 
-def symbol_uci_move(node):
-    move = split_uci_moves(node.move.uci())
-    piece_symbol = node.board().piece_at(chess.parse_square(move)).unicode_symbol()
-    return f"{piece_symbol} {move}"
 
 class GameAnalysis(FileManager):
 
@@ -207,11 +178,14 @@ class GameAnalysis(FileManager):
         self.is_game_processed = False
 
     def game_analysis(self):
-        from chesspuzzler.logger import log_board
+        from chesspuzzler.analysis.logger import log_board
+        # board_logger = configure_board_log(__name__, "next.log")
         board = chess.Board()
         engine = SimpleEngine.popen_uci(Constant.ENGINE_PATH)
         # The initial score at the begining of the game for WHITE is Cp 20
         prevScore = PovScore(Cp(20), board.turn)
+        prevpovScore = prevScore
+
         game_data = []
         column_labels = ["move_number", "move", "side", "fen", "cp", "wdl", "mate", "evaluation"]
 
@@ -223,25 +197,29 @@ class GameAnalysis(FileManager):
                 break
             board.push(node.move)
             board_info = BoardInfo(node)
-
             if node.eval():
                 info = node.eval()
             else:
                 info = engine.analyse(board, chess.engine.Limit(depth=Constant.SCAN_ENGINE_DEPTH))
                 GameAnalysis.set_node_details(node, info["score"])
 
-            evaluate = EvaluationEngine(node, engine, info, prevScore, board_info.turn)
+            evaluate = EvaluationEngine(node, engine, info, prevScore, prevpovScore, board_info.turn)
             evaluation = evaluate.position_eval()
+    
             # After evaluating the board position update the `prevScore`
+            prevpovScore = prevScore
             try:
                 prevScore = info["score"]
             except TypeError:
                 prevScore = info
             cp, wdl, mate = evaluate.current.cp, evaluate.current.wdl, evaluate.current.mate
             move_info = board_info.get_info() + [cp, wdl, mate, evaluation]
-            # print(log_board(board_info, node, prevScore.pov(board_info.turn).mate() if mate else cp, evaluation))
-            print(log_board(board_info, node, prevScore.pov(node.turn), evaluation))
+            # board_logger.info(log_board(board_info, node, prevScore.pov(node.turn), evaluation))
+            # print(log_board(board_info, node, prevScore.pov(node.turn), evaluation))
+            logger.info(log_board(board_info, node, prevScore.pov(node.turn), evaluation))
+            logger.debug("--"*20)
             game_data.append(move_info)
+
 
         df = pd.DataFrame(game_data, columns=column_labels)
         self.update_game(node.game(), node.game().headers.get("Site"))
