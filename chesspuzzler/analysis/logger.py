e0db13febdd5c99d5048610c19bf4b0af5b6cb21
Gideon Oba <deyonoba@gmail.com>
2024-05-09 13:59:15 +0100

    log in the full move number from board info

--- chesspuzzler/analysis/logger.py
+++ chesspuzzler/analysis/logger.py
@@ -42,14 +42,14 @@ def log_board(boardinfo: BoardInfo, node, cp, evaluation):
         map_colour = {"Blunder": Fore.RED, "Mistake": Fore.LIGHTYELLOW_EX, "Inaccuracy": Fore.LIGHTCYAN_EX}
         move_symbol = map_colour[evaluation] + symbol_uci_move(node) + map_nag[evaluation] + Fore.RESET
         evaluation = map_colour[evaluation] + evaluation + Fore.RESET
-        list_ = [boardinfo.half_move_number, boardinfo.side, move_symbol, cp, evaluation, "\n"+boardinfo.fen]
+        list_ = [boardinfo.fullmove_number, boardinfo.half_move_number, boardinfo.side, move_symbol, cp, evaluation, "\n"+boardinfo.fen]
         list_ = list(map(str, list_))
         message += "\t".join(list_)
 
     else:
         move_symbol = symbol_uci_move(node)
         evaluation = Fore.GREEN + evaluation + Fore.RESET
-        list_ = [boardinfo.half_move_number, boardinfo.side, move_symbol,cp, evaluation]
+        list_ = [boardinfo.fullmove_number, boardinfo.half_move_number, boardinfo.side, move_symbol,cp, evaluation]
         list_ = list(map(str, list_))
         message += "\t".join(list_)      
     return(message)
